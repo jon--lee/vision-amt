50a00af89e00ad3e0fb8d28fd503b8ffdd04102a
Michael Laskey <michaelluskey@michaels-MacBook-Pro-7.local>
2016-02-12 13:06:37 -0800

    checking error

--- test_net.py
+++ test_net.py
@@ -5,6 +5,7 @@ import cv2
 from Net.tensor import inputdata, net3
 import IPython
 import numpy as np
+import np.linalg as LA
 
 
 class NetTest(object):
@@ -13,8 +14,14 @@ class NetTest(object):
         self.tf_net = net3.NetThree()
         self.tf_net_path = net_path
         self.sess = self.tf_net.load(var_path=self.tf_net_path)
+        self.vals = []
 
-
+    def computerER(self):
+        sm = 0.0
+        for val in self.vals):
+            sm += val
+        print "AVERAGE SQUARED EUCLIDEAN LOSS ",sm/len(self.vals)
+        
     def rollCall(self):
         train_path = AMTOptions.train_file
         test_path = AMTOptions.test_file
@@ -22,8 +29,6 @@ class NetTest(object):
 
         print "Moving deltas from " + deltas_path + " to train: " + train_path + " and test: " + test_path
 
-        train_file = open(train_path, 'w+')
-        test_file = open(test_path, 'w+')
         deltas_file = open(deltas_path, 'r')
 
         for line in deltas_file:            
@@ -33,8 +38,15 @@ class NetTest(object):
             deltas_t = labels[1:5]
             img = cv2.imread(img_name,0)
             img = np.reshape(img, (250, 250, 1))
-            print "TRUE DELTAS ", deltas_t
-            print "NET DELTAS ", self.getNetOutput(img)
+            net_v = np.array(self.getNetOutput(img))
+            true_v = np.array(deltas_t)
+            err = 0.5*LA.norm(net_v-true_v)**2
+            self.vals.append(err)
+
+        self.computerER()
+
+
+            
 
     def getNetOutput(self,img):
         return self.tf_net.output(self.sess, img)
