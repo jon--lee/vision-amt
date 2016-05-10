192e4c6f3a76f0a5f8faa27a0fef5d6e5ff0e8b3
Michael Laskey <michaelluskey@airbears2-10-142-33-33.airbears2.1918.berkeley.edu>
2016-05-10 15:20:02 -0700

    added classification

--- test_tf.py
+++ test_tf.py
@@ -1,11 +1,11 @@
 from Net.tensor import net3
 from Net.tensor import net4
-from Net.tensor import net6,net8
+from Net.tensor import net6,net6_c,net8
 from Net.tensor import inputdata
 from options import AMTOptions
 
 data = inputdata.AMTData(AMTOptions.train_file, AMTOptions.test_file,channels=3)
-net = net6.NetSix()
+net = net6_c.NetSix_C()
 path = '/media/1tb/Izzy/nets/net6_05-04-2016_15h59m29s.ckpt'
 #path = '/media/1tb/Izzy/nets/net6_05-04-2016_14h10m59s.ckpt'
 # net.optimize(200,data,path = path, batch_size=200)
